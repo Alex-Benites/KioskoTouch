842bd623ae306ebcb6e8cec87fe33c16fbbc99b8
Johrespi <johrespi@espol.edu.ec>
2025-05-19 14:12:15 -0500

    Add: Angular material dependency

--- Frontend/src/app/administrador/productos/crear/crear.component.ts
+++ Frontend/src/app/administrador/productos/crear/crear.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderAdminComponent } from '../../../shared/header-admin/header-admin.component';
+import { FooterAdminComponent } from '../../../shared/footer-admin/footer-admin.component';
 @Component({
   selector: 'app-crear',
-  imports: [],
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule,
+    HeaderAdminComponent,
+    FooterAdminComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './crear.component.html',
   styleUrl: './crear.component.css'
 })
--- Frontend/src/app/shared/footer-admin/footer-admin.component.ts
+++ Frontend/src/app/shared/footer-admin/footer-admin.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import {Location} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer-admin',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer-admin.component.html',
   styleUrl: './footer-admin.component.css'
 })
 export class FooterAdminComponent {
 
-  constructor(private router: Router) {}
+
+  @Input() backRoute?: string | any[];
+
+  constructor(private router: Router, private location: Location) {}
 
   volver() {
-    this.router.navigate(['administrador/home']); 
+      if (this.backRoute) {
+      const routeToNavigate = Array.isArray(this.backRoute) ? this.backRoute : [this.backRoute];
+      this.router.navigate(routeToNavigate);
+    } else {
+      this.location.back();
+    }
   }
 
 }
