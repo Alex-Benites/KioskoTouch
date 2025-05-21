6638f24e0ea2bf232cf8f0fe16349a99b68d1987
Johrespi <johrespi@espol.edu.ec>
2025-05-20 22:18:17 -0500

    feat: pantalla publicidad

--- Frontend/src/app/administrador/publicidad/publicidad.component.ts
+++ Frontend/src/app/administrador/publicidad/publicidad.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderAdminComponent } from '../../shared/header-admin/header-admin.component';
+import { Router } from '@angular/router';
+import { FooterAdminComponent } from '../../shared/footer-admin/footer-admin.component';
 
 @Component({
   selector: 'app-publicidad',
-  imports: [HeaderAdminComponent],
+  imports: [HeaderAdminComponent, FooterAdminComponent],
   templateUrl: './publicidad.component.html',
   styleUrl: './publicidad.component.scss'
 })
 export class PublicidadComponent {
 
+  constructor(private router: Router) {}
+
+  irA(ruta: string) {
+    this.router.navigateByUrl(ruta);
+  }
 }
