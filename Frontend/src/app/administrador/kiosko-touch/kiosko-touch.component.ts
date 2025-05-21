db4fcda9cebbe9753344d478a886ad14c66bcbc5
Johrespi <johrespi@espol.edu.ec>
2025-05-20 22:29:38 -0500

    feat: pantalla kiosko-touch

--- Frontend/src/app/administrador/kiosko-touch/kiosko-touch.component.ts
+++ Frontend/src/app/administrador/kiosko-touch/kiosko-touch.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderAdminComponent } from '../../shared/header-admin/header-admin.component';
+import { Router } from '@angular/router';
+import { FooterAdminComponent } from '../../shared/footer-admin/footer-admin.component';
 
 @Component({
   selector: 'app-kiosko-touch',
-  imports: [HeaderAdminComponent],
+  imports: [HeaderAdminComponent, FooterAdminComponent],
   templateUrl: './kiosko-touch.component.html',
   styleUrl: './kiosko-touch.component.scss'
 })
 export class KioskoTouchComponent {
+
+  constructor(private router: Router) {}
+
+  irA(ruta: string) {
+    this.router.navigateByUrl(ruta);
+  }
 
 }
