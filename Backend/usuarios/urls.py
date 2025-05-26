24efb81c08fd989856a13d6c66887c8700ecd5db
Johrespi <johrespi@espol.edu.ec>
2025-05-25 20:07:33 -0500

    Add: urls for auth endpoints

--- Backend/usuarios/urls.py
+++ Backend/usuarios/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('auth/login/', views.login_empleado, name='login_empleado'),
+    path('auth/login/', views.login_empleado, name='login_empleado'),
+    path('auth/verify/', views.verify_token, name='verify_token'),
+    path('auth/logout/', views.logout_empleado, name='logout_empleado'),
+    path('auth/permissions/', views.get_user_permissions_endpoint, name='get_permissions'),
 ]
