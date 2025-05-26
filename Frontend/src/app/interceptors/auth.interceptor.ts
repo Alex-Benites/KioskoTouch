e07cb020fb8db742e8d54000d58cc31e40ccf1b9
Johrespi <johrespi@espol.edu.ec>
2025-05-25 21:52:39 -0500

    add: auth interceptor to add jwt token every api call

---
+++ Frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { inject } from '@angular/core';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const token = authService.getAccessToken();
+  
+  let authReq = req;
+  if (token && !req.url.includes('/auth/login/')) {
+    authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        console.warn('Token inválido o expirado. Redirigiendo al login...');
+        authService.logout().subscribe();
+      }
+      return throwError(() => error);
+    })
+  );
+};
