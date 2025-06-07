07ea90ebf13403e01d696f749abc366d11ec8734
Johrespi <johrespi@espol.edu.ec>
2025-06-07 17:43:03 -0500

    fix: cargar imagenes en produccion

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://johrespi.pythonanywhere.com/api',
-  baseUrl: 'https://johrespi.pythonanywhere.com/static'
+  baseUrl: 'https://johrespi.pythonanywhere.com'
 };
