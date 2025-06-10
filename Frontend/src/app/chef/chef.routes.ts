c50fc90855a676fdd15ad35153689856b17ec3cb
Johrespi <johrespi@espol.edu.ec>
2025-06-09 23:20:46 -0500

    feat: Modulo chef terminado [Frontend]

--- Frontend/src/app/chef/chef.routes.ts
+++ Frontend/src/app/chef/chef.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
+import { authGuard } from '../guards/auth.guard';
 
 export const chefRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'pedidos', component: PedidosComponent},
-  { path: '', redirectTo: 'pedidos', pathMatch: 'full'}
+  { path: 'pedidos', component: PedidosComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: 'pedidos', pathMatch: 'full'},
 ];
